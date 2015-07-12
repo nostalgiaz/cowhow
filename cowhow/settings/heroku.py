4f645cd66ad0fe91fc271bc2de1d4bc465dfcdf8
Patrick Arminio <patrick.arminio@gmail.com>
2015-07-12 11:37:27 +0200

    Static files.

--- cowhow/settings/heroku.py
+++ cowhow/settings/heroku.py
@@ -17,7 +17,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
