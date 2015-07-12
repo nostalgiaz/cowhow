cd1a27769288bda21564fb38ed679383dc0772c3
Patrick Arminio <patrick.arminio@gmail.com>
2015-07-12 08:58:35 +0200

    Update es when tables are saved.

--- ch_coworking/signals.py
+++ ch_coworking/signals.py
@@ -1,16 +1,14 @@
 from django.conf import settings
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
+
 from ch_coworking.helpers import ChHelpers
-from ch_coworking.models import Coworking
+from ch_coworking.models import Coworking, Table
 
 
-@receiver(m2m_changed, sender=Coworking.amenities.through,
-          dispatch_uid='ch_coworking.signals.coworking_m2m_save')
-@receiver(post_save, sender=Coworking,
-          dispatch_uid='ch_coworking.signals.coworking_post_save')
-def coworking_post_save(sender, instance, **kwargs):
+def insert_coworking(instance):
     client = ChHelpers.get_es()
+
     try:
         client.delete(
             index=settings.ELASTICSEARCH['index'],
@@ -19,6 +17,7 @@ def coworking_post_save(sender, instance, **kwargs):
         )
     except:
         pass
+
     client.index(
         index=settings.ELASTICSEARCH['index'],
         doc_type=settings.ELASTICSEARCH['doc_type'],
@@ -38,3 +37,17 @@ def coworking_post_save(sender, instance, **kwargs):
             } for t in instance.tables.all()]
         }
     )
+
+
+@receiver(m2m_changed, sender=Coworking.amenities.through,
+          dispatch_uid='ch_coworking.signals.coworking_m2m_save')
+@receiver(post_save, sender=Coworking,
+          dispatch_uid='ch_coworking.signals.coworking_post_save')
+def coworking_post_save(sender, instance, **kwargs):
+    insert_coworking(instance)
+
+
+@receiver(post_save, sender=Table,
+          dispatch_uid='ch_coworking.signals.table_post_save')
+def table_post_save(sender, instance, **kwargs):
+    insert_coworking(instance.coworking)
