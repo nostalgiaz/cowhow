b3efa8595e3423522a66f468792a22abbc02a8f3
Patrick Arminio <patrick.arminio@gmail.com>
2015-07-12 11:26:19 +0200

    Heroku debug false.

--- cowhow/settings/heroku.py
+++ cowhow/settings/heroku.py
@@ -11,6 +11,8 @@ DATABASES['default'] = dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+DEBUG = False
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
