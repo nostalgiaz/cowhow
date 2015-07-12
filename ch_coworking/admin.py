e8e715d8bfb6b4568a86d3c2725d79a42b60381c
Patrick Arminio <patrick.arminio@gmail.com>
2015-07-12 04:25:56 +0200

    Admin for photos. 🎠:

--- ch_coworking/admin.py
+++ ch_coworking/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Amenity, Coworking, Table, Reservation
+from .models import Amenity, Coworking, CoworkingPhoto, Table, Reservation
 
 admin.site.register(Amenity)
 admin.site.register(Coworking)
+admin.site.register(CoworkingPhoto)
 admin.site.register(Table)
 admin.site.register(Reservation)
